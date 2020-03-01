8e4cedd3e2965664d790310e88a110be52d0993b
Renato Richard <renatorichard2012@gmail.com>
2020-03-01 00:16:48 -0300

    first commit

---
+++ src/main/java/com/example/eba/Teste.java
@@ -0,0 +1,9 @@
+package com.example.eba;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("dsdsd"); ;
+    }
+
+}
---
+++ src/test/java/com/example/eba/EbaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
