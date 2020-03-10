783f240047cf5ade4452bd0bbe9d63cebbd85e13
Renato Richard <renatorichard2012@gmail.com>
2020-03-10 06:15:50 -0300

    updating person with service

--- src/main/java/com/example/eba/resource/PessoaResource.java
+++ src/main/java/com/example/eba/resource/PessoaResource.java
@@ -3,10 +3,8 @@ package com.example.eba.resource;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.eba.event.RecursoCriadoEvent;
 import com.example.eba.model.Pessoa;
 import com.example.eba.repository.PessoaRepository;
+import com.example.eba.service.PessoaService;
 
 
 @RestController
@@ -30,6 +29,8 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	@Autowired
+	private PessoaService pessoaService;
 	//Disparando um evento de aplicação
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -55,15 +56,8 @@ public class PessoaResource {
 	@PutMapping("/{codigo}")
 	//retorne a entidade Pessoa, com o codigo da pessoa dessa pessoa que quero atualizar
 	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
-		Pessoa pessoaSalva = pessoaRepository.findById(codigo).orElseThrow(() -> new EmptyResultDataAccessException(1));
-		if (pessoaSalva == null) {
-			//Lance essa execação em casos de não houver encontrado a referencia
-			  throw new EmptyResultDataAccessException(1);
-		}
-		//copie essa propriedade de atualização(font, qual propriedade, ignorado o codigo)
-		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
-		pessoaRepository.save(pessoaSalva);
-		return ResponseEntity.ok(pessoa);
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
 	}
 
 }
---
+++ src/main/java/com/example/eba/service/PessoaService.java
@@ -0,0 +1,28 @@
+package com.example.eba.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.eba.model.Pessoa;
+import com.example.eba.repository.PessoaRepository;
+
+//Essa classe será um compenente spring
+@Service
+public class PessoaService {
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaRepository.findById(codigo).orElseThrow(() -> new EmptyResultDataAccessException(1));
+		if (pessoaSalva == null) {
+			//Lance essa execação em casos de não houver encontrado a referencia
+			  throw new EmptyResultDataAccessException(1);
+		}
+		//copie essa propriedade de atualização(font, qual propriedade, ignorado o codigo)
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+
+}
