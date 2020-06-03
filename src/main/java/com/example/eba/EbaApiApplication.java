a416cad6745684184953a920b6a1d0fcc256f70a
Renato Richard <renatorichard2012@gmail.com>
2020-06-02 22:59:10 -0300

    main merges

--- src/main/java/com/example/eba/EbaApiApplication.java
+++ src/main/java/com/example/eba/EbaApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EbaApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EbaApiApplication.class, args);
 	}
